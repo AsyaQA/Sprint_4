16ad8b7534260a772657af74cde29d06ac3766b9
Anastasiia Turnaeva <asyutaisaeva@gmail.com>
2024-10-22 10:27:45 +0200

    Write tests for the class BooksCollector

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -22,3 +24,110 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_add_one_book_book_not_in_books_genre_and_len_book_from_1_to_40_add_book(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Русалочка')
+
+        assert len(collector.books_genre) == 1
+
+    def test_set_book_genre_book_in_books_genre_and_genre_in_genre_set_book_genre(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Шерлок Холмс')
+        collector.set_book_genre('Шерлок Холмс', 'Детективы')
+
+        assert collector.books_genre == {'Шерлок Холмс': 'Детективы'}
+
+
+    def test_get_book_genre_genre_in_genre_get_book_genre(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('На чужой земле')
+        collector.set_book_genre('На чужой земле', 'Фантастика')
+
+        assert collector.get_book_genre('На чужой земле') == 'Фантастика'
+
+
+    def test_get_books_with_specific_genre_book_in_books_genre_and_genre_in_genre_get_books_with_specific_genre(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Властелин колец')
+        collector.add_new_book('Хоббит')
+        collector.add_new_book('Русалочка')
+        collector.set_book_genre('Властелин колец', 'Фантастика')
+        collector.set_book_genre('Хоббит', 'Фантастика')
+        collector.set_book_genre('Русалочка', 'Мультфильмы')
+
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Властелин колец', 'Хоббит']
+
+
+    @pytest.mark.parametrize('name', ['Долг легиона', 'Курсант'])
+    def test_get_books_genre_one_book_no_genre_get_books_genre(self, name):
+
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+
+        assert collector.get_books_genre() == collector.books_genre
+
+
+    def test_get_books_for_children_two_books_genre_not_in_genre_age_rating_get_books_for_children(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Хоббит')
+        collector.add_new_book('Русалочка')
+        collector.set_book_genre('Хоббит', 'Фантастика')
+        collector.set_book_genre('Русалочка', 'Мультфильмы')
+
+        assert collector.get_books_for_children() == ['Хоббит', 'Русалочка']
+
+
+    def test_add_book_in_favorites_add_one_book_out_of_two_add_book_in_favorites(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Охотник на ведьм')
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Война и мир')
+
+        assert 'Война и мир' in collector.favorites
+
+
+    def test_delete_book_from_favorites_delete_one_book_delete_book_from_favorites(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Золушка')
+        collector.add_book_in_favorites('Золушка')
+        collector.delete_book_from_favorites('Золушка')
+
+        assert len(collector.favorites) == 0
+
+
+    def test_get_list_of_favorites_books_add_two_book_get_list_of_favorites(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Кот в сапогах')
+        collector.add_new_book('Красная шапочка')
+        collector.add_book_in_favorites('Кот в сапогах')
+        collector.add_book_in_favorites('Красная шапочка')
+
+        assert collector.get_list_of_favorites_books() == ['Кот в сапогах', 'Красная шапочка']
+
+
+    @pytest.mark.parametrize('name', ['', 'Жареные зеленые помидоры в кафе "Полустанок"'])
+    def test_add_new_book_add_book_len_name_0_and_len_name_more_40_not_add_book(self, name):
+
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+
+        assert len(collector.books_genre) == 0
